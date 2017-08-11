bf4e3b7f9c7c0723997ba90e14a941b35d9d956c
Anthony Nguyen <anknguyen@gmail.com>
2017-08-11 13:24:59 -0400

    Fetch pull requests in parallel with rayon

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 extern crate config;
 #[macro_use]
 extern crate error_chain;
+extern crate rayon;
 extern crate reqwest;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::process::exit;
 
 mod api;
@@ -20,19 +22,21 @@ mod types;
 
 fn run() -> Result<()> {
     let sets = settings::Settings::new()?;
+
     let api = Api::new(
         sets.github_api_token,
         sets.github_host
     )?;
 
     let repos = api.list_repos(&sets.github_subject)?;
-    let prs: Vec<types::PullRequest> = repos.iter()
+    let prs: Vec<types::PullRequest> = repos.par_iter()
         .flat_map(|repo| api.list_pull_requests(&repo.full_name))
         .flat_map(|ve| ve)
         .collect();
 
     for pr in &prs {
-        println!("[{}] {} ({})\n{}\n", pr.base.repo.full_name, pr.title, pr.state, pr.user.login);
+        println!("[{}] {} ({})\n{}\n", pr.base.repo.full_name, pr.title,
+            pr.state, pr.user.login);
     }
 
     Ok(())
